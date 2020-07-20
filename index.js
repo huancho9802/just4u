03a9e61ef28100e7b4f2eaf7228827e44692d01d
huancho9802 <gawdymoney.98@gmail.com>
2020-07-20 10:23:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.resolve(__dirname+'/client/build/index.html'));
 });
 
 app.listen(serverPort, function () {
