24f0956ad53d68ae67b7c5d791bff93d0df18a33
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2020-08-03 00:57:09 -0400

    model-test

--- models/user-model.js
+++ models/user-model.js
@@ -7,7 +7,6 @@ const User = new Schema(
     lastName: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    provider: String,
     city: String,
     state: { type: String, required: true },
     verification: {
@@ -24,3 +23,14 @@ const User = new Schema(
 );
 
 module.exports = mongoose.model("users", User);
+
+const Case = new Schema(
+    {
+        name: { type: String, required: true },
+        status: { type: String, required: true },
+        isOpened: { type: Boolean, required: true },
+        contact: { type: String, required: true }
+    }
+);
+
+module.exports = mongoose.model("cases", Case);
